22f54935f16ed6e066b11e88e7ce7c5b8e678c8e
Revan Abbaszade <revanabbaszade00@gmail.com>
2023-06-24 12:37:40 +0400

    Ragdoll Game

---
+++ Assets/scripts/Balance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Balance : MonoBehaviour
+{
+    public float targetRotation;
+    public Rigidbody2D rb;
+    public float force;
+    public GameObject restartButton;
+ 
+
+    public void Update()
+    {
+        rb.MoveRotation(Mathf.LerpAngle(rb.rotation, targetRotation, force * Time.fixedDeltaTime)); 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Asit")
+        {
+            Time.timeScale = 0;
+            restartButton.SetActive(true);
+            Debug.Log("xiyarrrrr");
+
+
+
+
+
+        }
+        if (collision.CompareTag("End"))
+        {
+            Debug.Log("Winn");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//level atlmaaaaa
+        }
+
+    }
+}
+
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float playerSpeed;
+    public Vector2 jumpHeight;
+    private bool isOnGround; public float positionRadius;
+    public LayerMask ground;
+    public Transform playerPos;
+
+
+
+
+    void Start()
+    {
+        Collider2D[] colliders = transform.GetComponentsInChildren<Collider2D>();
+        for (int i = 0; i < colliders.Length; i++)
+        { 
+            for (int k = i + 1; k < colliders.Length; k++)
+            {
+                Physics2D.IgnoreCollision(colliders[i], colliders[k]);
+            }
+        }
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal")!=0)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                anim.Play("Walk");
+                rb.AddForce(Vector2.right * playerSpeed *Time.deltaTime);
+            }
+            else
+            {
+                anim.Play("WalkBack");
+                rb.AddForce(Vector2.left * playerSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            anim.Play("idle");
+        }
+
+        isOnGround = Physics2D.OverlapCircle(playerPos.position, positionRadius, ground);
+        if (isOnGround==true && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("jumping");
+            rb.AddForce(Vector2.up * jumpForce * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Asit")
+        {
+            Time.timeScale = 0;
+            Debug.Log("xiyarrrrr");
+
+        
+
+            if (collision.gameObject.CompareTag("End"))
+            {
+                Debug.Log("End");
+
+
+            }
+
+           
+
+
+        }
+
+
+    }
+
+}
